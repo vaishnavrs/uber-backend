ab798b64b0db9b3783633bee94fcef9391d1ccbd
vaishnavrs <vaishnavramesh9@gmail.com>
2025-07-13 22:50:29 +0530

    Added Render domain to ALLOWED_HOSTS

--- uber/settings.py
+++ uber/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&=w6kzv$o0o5pqo1hpp&xazjd&j@_*$(&y#*rd%38mq3mv$o0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['uber-clone-ck0w.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['uber-backend-hqx7.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
@@ -157,3 +158,5 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = []
+
+SECRET_KEY = 'django-insecure--&=w6kzv$o0o5pqo1hpp&xazjd&j@_*$(&y#*rd%38mq3mv$o0'
