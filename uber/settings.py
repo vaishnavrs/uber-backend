f2654be83311592006becad545b0c9562c270301
vaishnavrs <vaishnavramesh9@gmail.com>
2025-07-13 23:01:26 +0530

    Added  allowed origins

--- uber/settings.py
+++ uber/settings.py
@@ -138,7 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://192.168.1.44:3000"
+    "http://192.168.1.44:3000",
+    "https://your-frontend-url.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
